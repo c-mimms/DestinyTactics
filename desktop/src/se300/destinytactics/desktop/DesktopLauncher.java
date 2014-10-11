bfde94aa6181553ad6d105b26f5451fce7ef60d8
Chris <christek13@gmail.com>
2014-10-11 15:34:32 -0400

    Created LibGdx

---
+++ desktop/src/se300/destinytactics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se300.destinytactics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se300.destinytactics.DestinyTactics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DestinyTactics(), config);
+	}
+}
---
+++ html/src/se300/destinytactics/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package se300.destinytactics.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import se300.destinytactics.DestinyTactics;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DestinyTactics();
+        }
+}
