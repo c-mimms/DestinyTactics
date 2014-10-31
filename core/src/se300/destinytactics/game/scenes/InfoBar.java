1af733858e29cf8f630e0e510bf24eb0bc02626c
Richard <Simonson>
2014-10-31 18:17:56 -0400

    Updated Infobar, made it perty. Trying a new way to get fleets with SelectBox, but I cant figure it out. All tutorials say to do it one way, but does not work when I try it.

--- core/src/se300/destinytactics/game/scenes/InfoBar.java
+++ core/src/se300/destinytactics/game/scenes/InfoBar.java
@@ -4,9 +4,15 @@ import se300.destinytactics.DestinyTactics;
 import se300.destinytactics.GameScene;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -16,10 +22,11 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class InfoBar extends Stage{
 
 	public GameScene myGame;
-	private Label label1;
-	private TextField txt1;
+	private Table infobar;
+	private Label label1, txt1, txt2, txt3, txt4,txt5, txt6;
 	public Skin skin;
 	TextButton PlayerButton, PlayerButton2, PlayerButton3;
+	SelectBox ListButton;
 	int edgePadding;
 	int buttonPadding = 5;
 	
@@ -31,19 +38,59 @@ public class InfoBar extends Stage{
 		edgePadding = padding;
 		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 		
-		label1 = new Label("infoBar", skin);
+		infobar = new Table(skin);
+		
+		label1 = new Label("Information", skin);
 		label1.setX(DestinyTactics.SCREEN_WIDTH / 2);
-		label1.setY(0);
+		label1.setY(100);
 		label1.setAlignment(Align.center);
 		
-		PlayerButton = new TextButton("Player Info", skin.get("default",
+		ListButton = new SelectBox(skin);
+		
+		PlayerButton = new TextButton("Selected Fleet", skin.get("default",
 				TextButtonStyle.class));
 		PlayerButton2 = new TextButton("Current game status", skin.get(
 				"default", TextButtonStyle.class));
-		PlayerButton3 = new TextButton("Current Fleet Status", skin.get(
+		PlayerButton3 = new TextButton("Fleet Status", skin.get(
 				"default", TextButtonStyle.class));
-		txt1 = new TextField("txt1", skin);
-
+		txt1 = new Label("txt1", skin);
+		txt2 = new Label("txt2", skin);
+		txt3 = new Label("txt3", skin);
+		txt4 = new Label("txt4", skin);
+		txt5 = new Label("txt5", skin);
+		txt6 = new Label("txt6", skin);
+		
+		infobar.setX(100);
+		infobar.setY(PlayerButton.getHeight());
+		PlayerButton.setX(0);
+        PlayerButton.setY(3*PlayerButton.getHeight());
+        PlayerButton2.setX(0);
+        PlayerButton2.setY(2*PlayerButton.getHeight());
+        PlayerButton3.setX(0);
+        PlayerButton3.setY(PlayerButton.getHeight());
+        ListButton.setX(0);
+        ListButton.setY(3*PlayerButton.getHeight());
+        txt1.setX(edgePadding * 10);
+        txt1.setY(PlayerButton.getHeight() * 3);
+        txt1.setText("Player Name");
+        txt2.setX(edgePadding * 10);
+        txt2.setY(PlayerButton.getHeight() * 2);
+        txt2.setText("Sector:");
+        txt3.setX(edgePadding * 10);
+        txt3.setY(PlayerButton.getHeight() * 1);
+        txt3.setText("selected fleet");
+        txt4.setX(edgePadding * 40);
+        txt4.setY(PlayerButton.getHeight() * 3);
+        txt4.setText("Travel Speed:");
+        txt5.setX(edgePadding * 40);
+        txt5.setY(PlayerButton.getHeight() * 2);
+        txt5.setText("Destination:");
+        txt6.setX(edgePadding * 40);
+        txt6.setY(PlayerButton.getHeight() * 1);
+        txt6.setText("Location:");
+		
+		
+		/*
 		PlayerButton.setX(edgePadding);
 		PlayerButton.setY(3 * PlayerButton.getHeight());
 		PlayerButton2.setX(edgePadding);
@@ -52,14 +99,28 @@ public class InfoBar extends Stage{
 		PlayerButton3.setY(PlayerButton.getHeight());
 		txt1.setX(DestinyTactics.SCREEN_WIDTH - txt1.getWidth() - edgePadding);
 		txt1.setY(edgePadding);
-		
+		*/
+        
+        
+        
+        
 		this.addActor(label1);
-		this.addActor(PlayerButton);
-		this.addActor(PlayerButton2);
-		this.addActor(PlayerButton3);
+		this.addActor(ListButton);
 		this.addActor(txt1);
+		this.addActor(txt2);
+		this.addActor(txt3);
+		this.addActor(txt4);
+		this.addActor(txt5);
+		this.addActor(txt6);
+		
+	}
+	
+	
+		
+		
+		
 	}
 		
-}
+
 
 	
