82a794ba1b425c900407e172c8450909b6475342
Richard Simonson <simonsr1@my.erau.edu>
2014-11-26 10:52:53 -0600

    Updated Test Code as requested by mike

---
+++ core/src/se300/destinytactics/game/fleet/JUnitRich.java
@@ -0,0 +1,36 @@
+package se300.destinytactics.game.fleet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitRich {
+
+	@Test
+	public void testSpaceToBuild() {
+		Battleship spaceAvailibleTest = new Battleship();
+		int result = spaceAvailibleTest.getSpaceToBuild();
+		if(result < 0){
+			fail("build cannot be less than zero");
+		}
+		else if(result > 1000){
+			fail("build time is taking too long");
+			
+		}
+	}
+		@SuppressWarnings("deprecation")
+		@Test
+		public void getbuildtimetest() {
+			Battleship buildtimetest = new Battleship();
+			int result = buildtimetest.getBuildTime();
+			if(result < 0){
+				fail("build cannot be less than zero");
+			}
+			if(result > 1){
+				fail("build time is taking too long");
+				
+			}
+	
+	}
+
+}
