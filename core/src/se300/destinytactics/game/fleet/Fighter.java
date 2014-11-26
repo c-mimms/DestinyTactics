8a96b10232fb2ff8814eb3eb858882d819f0af6f
Mike Miller <millem42@my.erau.edu>
2014-11-25 21:16:02 -0500

    my Junit class

--- core/src/se300/destinytactics/game/fleet/Fighter.java
+++ core/src/se300/destinytactics/game/fleet/Fighter.java
@@ -15,11 +15,18 @@ public class Fighter extends Ship implements needsHangar {
 	private int hangarcost = 1;
 	private int sectorTravelSpeed = 1;
 	private int buildTime;
+	private String name;
+	private boolean hangared = false;
 
 	public static ShipJSON stats = new ShipJSON();
 	
 	public Fighter(){
+		name = stats.unit;
 		buildTime = stats.buildTime;
+	}
+	
+	public Fighter(String names){
+		name = names;
 	}
 
 	public void finalize() throws Throwable {
@@ -33,8 +40,8 @@ public class Fighter extends Ship implements needsHangar {
 		return 0;
 	}
 
-	public boolean isHangared(){
-		return false;
+	public boolean getHangared(){
+		return hangared;
 	}
 
 	public int getHangarCost(){
@@ -43,7 +50,7 @@ public class Fighter extends Ship implements needsHangar {
 	
 	@Override
 	public String getShipType(){
-		return stats.unit;
+		return name;
 	}
 	
 
@@ -61,11 +68,25 @@ public class Fighter extends Ship implements needsHangar {
 	@Override
 	public void decrementBuildTime() {
 		buildTime--;
-		
 	}
+	
 	@Override
 	public int getBuildTime() {
 		return buildTime;
+	}
+	
+	/**
+	 * Set hangared to true or false
+	 * @param hangared
+	 */
+	public void setHangared(boolean hangared){
+		this.hangared = hangared;
+	}
+
+	@Override
+	public boolean isHangared() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }//end Fighter
